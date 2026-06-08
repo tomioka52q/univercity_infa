da479bf5f01103fb3da4079e0b97a619d3c876da
tomioka52q <androcube07@gmail.com>
2026-06-08 20:10:45 +0300

    Update divide and conquer.cpp

--- Laba 7/divide and conquer.cpp
+++ Laba 7/divide and conquer.cpp
@@ -3,22 +3,26 @@
 
 using namespace std;
 
-int findPeak(const vector<int>& a, int n) {
-    int left = 0;
-    int right = n - 1;
-
-    while (left < right) {
-        int mid = left + (right - left) / 2;
-
-        // Сравниваем средний элемент со следующим
-        if (a[mid] < a[mid + 1]) {
-            left = mid + 1;
-        }
-        else {
-            right = mid;
-        }
+int findPeakRecursive(const vector<int>& a, int left, int right) {
+    if (left >= right) {
+        return a[left];
     }
-    return a[left];
+
+    int mid = left + (right - left) / 2;
+
+    // Сравниваем средний элемент со следующим
+    if (a[mid] < a[mid + 1]) {
+        return findPeakRecursive(a, mid + 1, right);
+    } else {
+        return findPeakRecursive(a, left, mid);
+    }
+}
+
+int findPeak(const vector<int>& a, int n) {
+    if (n == 1) {
+        return a[0];
+    }
+    return findPeakRecursive(a, 0, n - 1);
 }
 
 int main() {
@@ -28,11 +32,6 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-    }
-
-    if (n == 1) {
-        cout << a[0] << endl;
-        return 0;
     }
 
     cout << findPeak(a, n) << endl;
